285e698f982cd0e986f68d14db4e4376bd0460dc
Ludovic Charleux <ludovic.charleux@univ-smb.fr>
2024-05-13 15:02:39 +0200

    Added example.

--- src/day_2/create_csvs.py
+++ src/day_2/create_csvs.py
@@ -13,3 +13,4 @@ x = np.linspace(0, 10, 1001)
 y = myfunc(x, 1, 0.2, 3) + np.random.normal(0, 0.1, x.size)
 df = pd.DataFrame({"x": x, "y": y})
 df.to_csv("data.csv", index=False)
+df.to_excel("data.xlsx", index=False)  # mamba install -y openpyxl xlrd xlwt xlsxwriter
---
+++ src/day_2/csv_processor.py
@@ -0,0 +1,31 @@
+# PROCESSS CSV FILE WITH A CLASS
+
+import pandas as pd
+import numpy as np
+from scipy import optimize
+
+
+def myfunc(x, a, b, c):
+    """
+    A math function.
+    """
+    return a * np.exp(-b * x) * np.sin(c * x)
+
+
+# data = pd.read_excel("data.xlsx", header=1)
+# data = pd.read_csv("data.csv")
+
+
+class CSVprocessor:
+    def __init__(self, path):
+        data = pd.read_csv(path)
+        self.data = data
+
+    def apply_fit(self):
+        data = self.data
+        params, cov = optimize.curve_fit(myfunc, data.x, data.y)
+        self.fit = params
+
+
+processor = CSVprocessor("data.csv")
+processor.apply_fit()
