100df8157652434f0763bc8ac2375bbbb1240dec
Ludovic Charleux <ludovic.charleux@univ-smb.fr>
2024-05-13 17:10:16 +0200

    updated batch processing

--- src/day_2/csv_processor.py
+++ src/day_2/csv_processor.py
@@ -5,8 +5,20 @@ import numpy as np
 from scipy import optimize
 import matplotlib.pyplot as plt
 from processing import myfunc, CSVprocessor
+import os
+
+processors = []
+files = os.listdir("./")
+for file in files:
+    if file.endswith(".csv"):
+        processor = CSVprocessor(file)
+        processor.apply_fit()
+        processors.append(processor)
+
+fits = np.array([processor.fit for processor in processors])
+fits = pd.DataFrame(fits, columns=list("abc"))
 
 
-processor = CSVprocessor("data.csv")
-processor.apply_fit()
-processor.plot("nice_fig.pdf")
+# processor = CSVprocessor("data.csv")
+# processor.apply_fit()
+# processor.plot("nice_fig.pdf")
